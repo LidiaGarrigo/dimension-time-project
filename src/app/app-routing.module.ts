638a7b5a0b03db370fd7366a387b07f7765cdcc4
= <=>
2021-01-02 17:26:40 +0100

    routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{
+    onSameUrlNavigation: 'reload',
+    scrollPositionRestoration: 'enabled',
+  })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
